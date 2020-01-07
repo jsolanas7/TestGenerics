8f221043fa6068f3c59a92a97ddc7c731293af56
Juan Andrés Solanas <jsolanas@mstech.la>
2020-01-07 09:23:52 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class ClaseA
+    {
+
+    }
+    public class MiClase<T>
+    {
+        T value;
+
+        public MiClase(T t)
+        {
+            this.value = t;
+        }
+
+        public void Write()
+        {
+            Console.WriteLine(this.value);
+        }
+    }
+}
